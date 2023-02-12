2c3e70a22f2d09eadfe2cc89310160a2a0ee11c3
WebDev717 <lasse.lin717@gmail.com>
2023-02-12 16:09:03 +0200

    fix casuel

--- src/components/ProductSlider/RekSubProductSlider.jsx
+++ src/components/ProductSlider/RekSubProductSlider.jsx
@@ -3,20 +3,33 @@ import picture1 from "./../../assets/images/dashboard/picture1.png";
 import picture2 from "./../../assets/images/dashboard/picture2.png";
 import picture3 from "./../../assets/images/dashboard/picture3.png";
 import VideoQuickView from "../QuickView/VideoQuickView";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { useRef } from "react";
 
 function RekSubProductSlider() {
   const [open, setOpen] = useState(false);
   const [flag, setFlag] = useState("flag1");
+  SwiperCore.use([Autoplay]);
 
   return (
     <Swiper
       spaceBetween={50}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       slidesPerView={3}
+      loop={true}
+      // autoplay
+      navigation
+      // pagination={{ clickable: true }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
       className="w-full gap-2 py-[4rem] mt-[50px]"
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide className="inline-block h-[350px] w-[450px] relative">
         <div className="flex bg-[#161616] h-full w-full p-[1rem] flex flex-col">
